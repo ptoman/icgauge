e1f280f0933186fa8d05398434fcc16223b0e7e2
Pamela <ptoman@stanford.edu>
2016-05-18 18:16:54 -0700

    Updates the entailment extraction utility code to include SICK data and some code to look at the PPDB data

--- icgauge/utils_entailment.py
+++ icgauge/utils_entailment.py
@@ -1,4 +1,4 @@
-# Parses the SNLI entailment data
+# Parses the entailment data
 #
 # Resulting format is complexity.jsonl, with the properties:
 #   label -- integer in {0,1}
@@ -9,6 +9,8 @@
 import json
 import codecs
 
+
+# SNLI
 with codecs.open('complexity.jsonl', 'w', 'utf-8') as f_out:
     for source_file in ['snli_1.0_dev.jsonl', 'snli_1.0_test.jsonl', 'snli_1.0_train.jsonl']:
         with codecs.open(source_file,'rU','utf-8') as f_in:
@@ -22,3 +24,39 @@ with codecs.open('complexity.jsonl', 'w', 'utf-8') as f_out:
                     f_out.write(out + '\n')
 
 
+# SICK
+with codecs.open('complexity.jsonl', 'w', 'utf-8') as f_out:
+    for source_file in ['SICK.txt']:
+        with codecs.open(source_file,'rU','utf-8') as f_in:
+            for line in f_in:
+                _, sentA, sentB, _, _, atob, btoa, _, _, _, _, _ = line.split('\t')
+                if atob == "A_entails_B" and btoa == "B_neutral_A":
+                    # A is more complex than B
+                    out = json.dumps({'label': 1, 'example': sentA})
+                    f_out.write(out + '\n')
+                    out = json.dumps({'label': 0, 'example': sentB})
+                    f_out.write(out + '\n')
+                if btoa == "B_entails_A" and atob == "A_neutral_B":
+                    # B is more complex than A
+                    out = json.dumps({'label': 1, 'example': sentB})
+                    f_out.write(out + '\n')
+                    out = json.dumps({'label': 0, 'example': sentA})
+                    f_out.write(out + '\n')
+                    
+
+# PPDB
+# This code just shows examples, illustrating that this dataset is on the
+# whole quite bidirectional.
+ctr = 0
+with codecs.open('complexity.jsonl', 'w', 'utf-8') as f_out:
+    for source_file in ['ppdb-1.0-s-all']:
+        with codecs.open(source_file,'rU','utf-8') as f_in:
+            for line in f_in:
+                _, sentA, sentB, _, _ = line.split(' ||| ')
+                combo = sentA + "___" + sentB
+                comboRev = sentB + "___" + sentA
+                ctr += 1
+                if ctr % 10000 == 0:
+                    print sentA
+                    print sentB
+                    print
